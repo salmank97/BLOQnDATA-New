b3a634e70c23e5385904a8d35341cb5b6bc3aa6f
salmank97 <salman5@live.com>
2019-04-03 15:57:23 +0500

    logos removed

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -206,7 +206,7 @@ function Banner(props) {
         
       </QueueAnim>
       {/* <Subscribe/> */}
-      <div className="partner-logos" >
+      {/* <div className="partner-logos" >
         <Row className="bottom-bar" >
         <Col xs = {8} sm={4}><img src = {partner} style = {{width : '100px'}}/></Col>      
         <Col xs = {8} sm={4}><img src = {partner} style = {{width : '100px'}}/></Col>
@@ -215,7 +215,7 @@ function Banner(props) {
         <Col xs = {8} sm={4}><img src = {partner} style = {{width : '100px'}}/></Col>      
         <Col xs = {8} sm={4}><img src = {partner} style = {{width : '100px'}}/></Col>
       </Row>
-        </div>
+        </div> */}
     </div>
   );
 }
